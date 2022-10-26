671ba036ab9ee965efca759f4d296046cc9a3c05
Sebastian Hernandez <sebhernandez@estudiantec.cr>
2022-10-26 00:28:46 -0600

    solucion git kraken

--- src/main/java/Algoritmos/testquick.java
+++ src/main/java/Algoritmos/testquick.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class testquick {
     public static void main(String[] args){
-        String arrayPrueba[] = {"hola", "adios", "comida", "avion", "xderwqer"};
+        String arrayPrueba[] = {"hola", "adios", "comida", "avion", "adrian"};
         QuickSort algoritmo = new QuickSort();
         algoritmo.sort(arrayPrueba);
         System.out.println(Arrays.toString(arrayPrueba));
